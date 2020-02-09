b25dcafe7126c691ec7e33802bcf9e22df1c928f
Floozutter <floozutter@gmail.com>
2020-02-09 15:01:18 -0800

    Remove unnecessary indents

--- python/fstringabuse.py
+++ python/fstringabuse.py
@@ -4,8 +4,8 @@ A one-liner abuse of Python f-strings.
 
 
 print(f"""Squares: {
-        ' '.join(map(
-            lambda n: str( int(input(f'#{n + 1}: '))**2 ),
-            range( int(input('# of #s to square: ')) )
-        ))
+    ' '.join(map(
+        lambda n: str( int(input(f'#{n + 1}: '))**2 ),
+        range( int(input('# of #s to square: ')) )
+    ))
 }""")
