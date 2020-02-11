060fa35dba9708242d638eacc4f2385297d10170
Floozutter <floozutter@gmail.com>
2020-02-11 15:01:06 -0800

    Accept any Iterable of Rules, not only Lists

--- python/fizzbuzzfunctional.py
+++ python/fizzbuzzfunctional.py
@@ -4,7 +4,7 @@ An extensible solution to the FizzBuzz problem, in a functional style.
 https://en.wikipedia.org/wiki/Fizz_buzz
 """
 
-from typing import Callable, NamedTuple, List
+from typing import Callable, NamedTuple, Iterable
 
 
 IntChecker = Callable[[int], bool]  # checks if an integer passes a condition
@@ -14,29 +14,29 @@ class Rule(NamedTuple):             # associates a subname to a condition
     condition: IntChecker 
 
 
-def make_namer(rules: List[Rule]) -> IntNamer:
+def make_namer(rules: Iterable[Rule]) -> IntNamer:
     """
-    Returns an IntNamer closure from a list of Rules.
+    Returns an IntNamer closure from an Iterable of Rules.
 
     The IntNamer will name an integer as a concatenation of every valid
-    subname it fulfills from the List of Rules.
+    subname it fulfills from the Iterable of Rules.
     
     The order of the subnames in an int's full name corresponds to the order
-    of the Rules in the List of Rules.
+    of the Rules in the Iterable.
     """
     
     def namer(z: int) -> str:
         """
         Names the integer argument using the bound List of Rules.
         """
-        # get the subnames the integer fulfills
-        subnames = [r.subname for r in rules if r.condition(z)]
+        # make the full name from the subnames the integer fulfills
+        name = "".join(r.subname for r in rules if r.condition(z))
         
-        if subnames:  # the integer fulfills at least one subname
-            return "".join(subnames)
+        if name:  # the integer fulfills at least one subname
+            return name
         
         # when the integer fulfills no subnames,
-        # name the integer by its value
+        # just name the integer by its value instead
         return str(z)
     
     return namer
@@ -44,10 +44,10 @@ def make_namer(rules: List[Rule]) -> IntNamer:
 
 if __name__ == "__main__":
     # make the classic FizzBizz namer
-    fizzbuzz = make_namer([
+    fizzbuzz = make_namer((
         Rule("Fizz", lambda z: z % 3 == 0),
         Rule("Buzz", lambda z: z % 5 == 0)
-    ])
+    ))
 
     # name the integers within the range [1, 100] with fizzbuzz, and print
     print("\n".join(map(fizzbuzz, range(1, 101))))
