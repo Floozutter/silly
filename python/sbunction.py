02b676280e5a4dbce0af61d60f3013fc5ed50fc4
Floozutter <floozutter@gmail.com>
2020-02-26 10:25:12 -0800

    Make sbunction callable with square brackets

--- python/sbunction.py
+++ python/sbunction.py
@@ -22,11 +22,22 @@ class Sbunction:
         if isinstance(key, tuple): return self._func(*key)
         else: return self._func(key)
 
+class Sbfunction(Sbunction):
+    """
+    Sbfunctions are Sbunctions that can also be called with parentheses.
+    """
+    def __init__(self, func: Callable):
+        Sbunction.__init__(self, func)
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+
 def sbunction(func: Callable):
     """
     Decorator to make a Sbunction out of a normal ol' funcion.
     """
     return Sbunction(func)
+sbunction = Sbfunction(sbunction)
 
 
 if __name__ == "__main__":
@@ -43,14 +54,17 @@ if __name__ == "__main__":
     @sbunction
     def scale(vector: Tuple[int, ...], scalar: int = 2) -> Tuple[int, ...]:
         return tuple(i*scalar for i in vector)
-    # Use a comma to avoid unpacking a single Tuple argument by itself!
+    # Use a comma to avoid unpacking a single Tuple argument!
     assert scale[(1, 2, 3),  ] == (2, 4, 6)
     assert scale[(142857,), 0] == (0,)
 
     @sbunction
     def sayhi() -> str:
         return "Hello World!"
-    # Use an empty Tuple to call with no arguments!
+    # Use an empty Tuple to call a Sbunction with no arguments!
     assert sayhi[()] == "Hello World!"
 
-    print("Passed all checks. Have sbun!")
+    # Use sbunction as a Sbunction to decorate an already existing function!
+    print = sbunction[print]
+
+    print["Passed all checks.", "Have sbun!"]
