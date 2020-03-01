9a92877dd6f9cb54f866949bf82d0f0dc2552e51
Floozutter <floozutter@gmail.com>
2020-02-29 22:31:31 -0800

    Make Sbunction callable with square brackets

--- python/sbunction.py
+++ python/sbunction.py
@@ -7,7 +7,19 @@ Sometimes, you want to call functions using square brackets instead.
 from typing import Any, Union, Callable, Tuple
 
 
-class Sbunction:
+class Sblass(type):
+    """
+    Classes of Sblass can be called with square brackets.
+    """
+    def __getitem__(self, key):
+        """
+        Passes the key argument to __call__.
+        Tuple arguments are always unpacked when passed.
+        """
+        if isinstance(key, tuple): return self.__call__(*key)
+        else: return self.__call__(key)
+
+class Sbunction(metaclass=Sblass):
     """
     Sbunctions are functions called with square brackets.
     """
@@ -22,49 +34,32 @@ class Sbunction:
         if isinstance(key, tuple): return self._func(*key)
         else: return self._func(key)
 
-class Sbfunction(Sbunction):
-    """
-    Sbfunctions are Sbunctions that can also be called with parentheses.
-    """
-    def __init__(self, func: Callable) -> None:
-        super().__init__(func)
-    def __call__(self, *args, **kwargs) -> Any:
-        return self._func(*args, **kwargs)
-
-
-@Sbfunction
-def sbunction(func: Callable) -> Sbunction:
-    """
-    Decorator to make a Sbunction out of a normal ol' funcion.
-    """
-    return Sbunction(func)
-
 
 if __name__ == "__main__":
-    @sbunction
+    @Sbunction
     def increment(z: int) -> int:
         return z+1
     assert increment[1] == 2
 
-    @sbunction
+    @Sbunction
     def add(a: int, b: int) -> int:
         return a+b
     assert add[1, 2] == 3
 
-    @sbunction
+    @Sbunction
     def scale(vector: Tuple[int, ...], scalar: int = 2) -> Tuple[int, ...]:
         return tuple(i*scalar for i in vector)
     # Use a comma to avoid unpacking a single Tuple argument!
     assert scale[(1, 2, 3),  ] == (2, 4, 6)
     assert scale[(142857,), 0] == (0,)
 
-    @sbunction
+    @Sbunction
     def sayhi() -> str:
         return "Hello World!"
     # Use an empty Tuple to call a Sbunction with no arguments!
     assert sayhi[()] == "Hello World!"
 
-    # Use sbunction as a Sbunction to decorate an already existing function!
-    print = sbunction[print]
+    # Call Sbunction with square brackets to decorate a predefined function!
+    print = Sbunction[print]
 
     print["Passed all checks.", "Have sbun!"]
