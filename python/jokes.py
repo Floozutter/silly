a7c83df5e9b3bba1cfc406ae7d7b137c45d46bd4
Floozutter <floozutter@gmail.com>
2020-04-01 15:10:43 -0700

    Add function that answers how you are

--- python/jokes.py
+++ python/jokes.py
@@ -1,3 +1,20 @@
 """
 Silly code that wasn't written to be ran.
 """
+
+from typing import AbstractSet, Mapping, Iterable, Any
+
+
+def how_are_you(
+    health: int,                             # Integer because health/points/.
+    hunger: int,                             # Is high hunger good or bad?
+    energy: int,                             # Boost using coffee.
+    mental: int,                             # Mine's negative.
+    status_ailments: AbstractSet[str],       # Stringly typed! >:O
+    relationship_scores: Mapping[str, int],  # No "Player ID" type?
+    current_thoughtstream: Iterable[str],    # I exhausted this.
+    mood: Any,                               # Mood.
+    is_actually_okay: bool = False           # :(
+) -> str:
+    """Returns an answer to the question "How are you?" using your state."""
+    return "Okay."
