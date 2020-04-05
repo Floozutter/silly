2da1023d81a43258e05a0ac73f7dfd83c11ed64c
Floozutter <floozutter@gmail.com>
2020-04-04 17:58:04 -0700

    Test if a str is a Sequence[str]

--- python/typelab.py
+++ python/typelab.py
@@ -1,3 +1,10 @@
 """
 Experiments with typing in Python!
 """
+
+from typing import Sequence
+
+
+# Is a str also a Sequence[str]?
+magic_string: str = "deadbeef"
+magic_strseq: Sequence[str] = "deadbeef"
