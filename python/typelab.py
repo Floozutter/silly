569e8378c6102f64aa62cb7d9bf4b2e4d379ac76
Floozutter <floozutter@gmail.com>
2020-04-05 18:21:47 -0700

    Test if a str is an Iterable[str]

--- python/typelab.py
+++ python/typelab.py
@@ -2,9 +2,12 @@
 Experiments with typing in Python!
 """
 
-from typing import Sequence
+from typing import Sequence, Iterable
 
 
 # Is a str also a Sequence[str]?
 magic_string: str = "deadbeef"
 magic_strseq: Sequence[str] = "deadbeef"
+
+# Is a str also an Iterable[str]?
+magic_stritr: Iterable[str] = "deadbeef"
