114b0c1b99fc2dbfc02b50b4cb11b7438a2bb8af
Floozutter <floozutter@gmail.com>
2020-04-05 18:22:36 -0700

    Test if an int is a float

--- python/typelab.py
+++ python/typelab.py
@@ -5,6 +5,10 @@ Experiments with typing in Python!
 from typing import Sequence, Iterable
 
 
+# Is an int also a float?
+magic_int: int = 3735928559
+magic_float: float = 3735928559
+
 # Is a str also a Sequence[str]?
 magic_string: str = "deadbeef"
 magic_strseq: Sequence[str] = "deadbeef"
