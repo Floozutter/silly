7718bc9056fea195b6ae3540fab5d843e39d0d9e
Floozutter <floozutter@gmail.com>
2020-04-18 13:47:30 -0700

    Add a memoized random function (subroutine)

--- python/jokes.py
+++ python/jokes.py
@@ -2,6 +2,8 @@
 Silly code that wasn't written to be ran.
 """
 
+import random
+import functools
 from typing import AbstractSet, Mapping, Iterable, Any
 
 
@@ -20,3 +22,9 @@ def how_are_you(
     Returns an answer to the question "How are you?" using your current state.
     """
     return "Okay."
+
+
+@functools.lru_cache(maxsize=1)
+def rand100() -> int:
+    """Returns a random integer within [0, 100)."""
+    return random.randrange(0, 100)
