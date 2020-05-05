3eb3b3a4ae18e6984bb1d00661fbd2446b7030b0
Floozutter <floozutter@gmail.com>
2020-05-05 12:25:32 -0700

    Update wrapping Sbunctions with update_wrapper

--- python/sbunction.py
+++ python/sbunction.py
@@ -4,6 +4,7 @@ Sometimes, you want something different, yet familiar.
 Sometimes, you want to call functions using square brackets instead.
 """
 
+from functools import update_wrapper
 from typing import Any, Union, Callable, Tuple
 
 
@@ -26,6 +27,7 @@ class Sbunction(metaclass=Sblass):
     _func: Callable  # under-the-hood function to call
     def __init__(self, func: Callable) -> None:
         self._func = func
+        update_wrapper(self, self._func)
     def __getitem__(self, key: Union[Tuple, Any]) -> Any:
         """
         Handles passing the key argument to the under-the-hood function.
@@ -38,16 +40,25 @@ class Sbunction(metaclass=Sblass):
 if __name__ == "__main__":
     @Sbunction
     def increment(z: int) -> int:
+        """Returns the argument incremented by 1."""
         return z+1
+    assert increment.__name__ == "increment"
+    assert increment.__doc__ == """Returns the argument incremented by 1."""
+    assert increment.__annotations__ == {
+        "z": int,
+        "return": int
+    }
     assert increment[1] == 2
 
     @Sbunction
     def add(a: int, b: int) -> int:
+        """Returns the sum of the two arguments."""
         return a+b
     assert add[1, 2] == 3
 
     @Sbunction
     def scale(vector: Tuple[int, ...], scalar: int = 2) -> Tuple[int, ...]:
+        """Returns the vector scaled by the scalar."""
         return tuple(i*scalar for i in vector)
     # Use a comma to avoid unpacking a single Tuple argument!
     assert scale[(1, 2, 3),  ] == (2, 4, 6)
@@ -55,6 +66,7 @@ if __name__ == "__main__":
 
     @Sbunction
     def sayhi() -> str:
+        """Returns a greeting to the world."""
         return "Hello World!"
     # Use an empty Tuple to call a Sbunction with no arguments!
     assert sayhi[()] == "Hello World!"
