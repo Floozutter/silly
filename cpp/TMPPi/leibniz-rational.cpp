1e86ce53292ac61bfc6d123ebdac1b5d256f7291
Floozutter <floozutter@gmail.com>
2020-08-18 02:07:42 -0700

    Use constexpr instead of const; add a space

--- cpp/TMPPi/leibniz-rational.cpp
+++ cpp/TMPPi/leibniz-rational.cpp
@@ -3,40 +3,40 @@
 
 template <unsigned long long int a, unsigned long long int b>
 struct GCD {
-	static const unsigned long long int value = GCD<b, a % b>::value;
+	static constexpr unsigned long long int value = GCD<b, a % b>::value;
 };
 template <unsigned long long int a>
 struct GCD<a, 0> {
-	static const unsigned long long int value = a;
+	static constexpr unsigned long long int value = a;
 };
 
 template <unsigned int n>
 struct QPI;
 template <unsigned int n>
 struct QPIE {
-	static const unsigned long long int num = (
+	static constexpr unsigned long long int num = (
 		(2*n + 1) * QPI<n-1>::num
 		+ QPI<n-1>::den * static_cast<unsigned long long int>(n % 2 ? -1 : 1)
 	);
-	static const unsigned long long int den = QPI<n-1>::den * (2*n + 1);
+	static constexpr unsigned long long int den = QPI<n-1>::den * (2*n + 1);
 };
 template <unsigned int n>
 struct QPI {
-	static const unsigned long long int num = (
+	static constexpr unsigned long long int num = (
 		QPIE<n>::num / GCD<QPIE<n>::num, QPIE<n>::den>::value
 	);
-	static const unsigned long long int den = (
+	static constexpr unsigned long long int den = (
 		QPIE<n>::den / GCD<QPIE<n>::num, QPIE<n>::den>::value
 	);
 };
 template <>
 struct QPI<0> {
-	static const unsigned long long int num = 1;
-	static const unsigned long long int	den = 1;
+	static constexpr unsigned long long int num = 1;
+	static constexpr unsigned long long int	den = 1;
 };
 
 constexpr unsigned int N = 23;
-constexpr double COMPI = 4* static_cast<double>(QPI<N>::num) / QPI<N>::den;
+constexpr double COMPI = 4 * static_cast<double>(QPI<N>::num) / QPI<N>::den;
 
 
 int main() {
