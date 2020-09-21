5586c6cecbf1bc20935b1e06bba1845a1b1cc874
Floozutter <floozutter@gmail.com>
2020-09-20 23:52:17 -0700

    Add row diff and col diff arrays tech

--- cpp/AdjacentCoordinates/main.cpp
+++ cpp/AdjacentCoordinates/main.cpp
@@ -20,5 +20,12 @@ int main() {
 	std::cout << grid[row-1][col] << std::endl;
 	std::cout << grid[row][col-1] << std::endl;
 	
+	std::cout << "Row difference and column difference arrays:" << std::endl;
+	int const dr[] = {1, 0, -1, 0};
+	int const dc[] = {0, 1, 0, -1};
+	for (size_t i = 0; i < 4; ++i) {
+		std::cout << grid[row + dr[i]][col + dc[i]] << std::endl;
+	}
+	
 	return 0;
 }
