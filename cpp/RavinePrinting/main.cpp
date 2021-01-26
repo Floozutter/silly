e954efbd63da4c9905331bf2ce77cb0fe8975a16
Floozutter <floozutter@gmail.com>
2021-01-26 14:33:45 -0800

    add RavinePrinting

---
+++ cpp/RavinePrinting/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+void print_data(double principal_amount, double interest_rate, double times_compounded) {
+    int const l{20}, r{10};
+    std::ostringstream a; a << '$' << principal_amount;
+    std::ostringstream b; b << interest_rate << '%';
+    std::ostringstream c; c << times_compounded;
+    using std::setw, std::left, std::right, std::endl;
+    std::cout
+        << left << setw(l) << "Principal Amount:" << right << setw(r) << a.str() << endl
+        << left << setw(l) << "Interest Rate:"    << right << setw(r) << b.str() << endl
+        << left << setw(l) << "Times Compounded:" << right << setw(r) << c.str() << endl;
+}
+
+int main() {
+    print_data(420.69, 3.14, 2.72);
+    return 0;
+}
