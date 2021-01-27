400c7d2713bd21523e54a970bfd031ed46f439bb
Floozutter <floozutter@gmail.com>
2021-01-27 00:57:38 -0800

    add ConstStream

---
+++ cpp/ConstStream/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+const long magic{3735928559};
+const long cigam{4276869869};
+
+void print_into_buffer(std::ostream const & out) {
+    std::ostream share{out.rdbuf()};
+    share << "uwu: " << std::hex << magic << ' ' << cigam << std::endl;;
+}
+
+int main() {
+    std::cout << "owo: " << magic << ' ' << cigam << std::endl;
+    print_into_buffer(std::cout);
+    std::cout << "owo: " << magic << ' ' << cigam << std::endl;
+}
