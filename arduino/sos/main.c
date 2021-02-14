f953cf50a9534cd7b1e85d40397fe338252665a5
Floozutter <floozutter@gmail.com>
2021-02-13 23:24:08 -0800

    start arduino with sos

---
+++ arduino/sos/main.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+const int DIT_LENGTH = 200;
+
+void wait(unsigned units) {
+    for (int i = 0; i < units; ++i) {
+        _delay_ms(DIT_LENGTH);
+    }
+}
+
+void on(unsigned units) {
+    PORTB |= 1 << PB5;
+    wait(units);
+}
+void off(unsigned units) {
+    PORTB &= ~(1 << PB5);
+    wait(units);
+}
+
+int main(void) {
+    DDRB |= 1 << DDB5;
+    while (1) {
+        on(1); off(1); on(1); off(1); on(1); off(1);  // S
+        on(3); off(1); on(3); off(1); on(3); off(1);  // O
+        on(1); off(1); on(1); off(1); on(1); off(1);  // S
+        off(6);
+    }
+    return 0;
+}
