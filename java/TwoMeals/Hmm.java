f2c3a93e2996c0ca15322ab4a624cb8a4e239921
Floozutter <me@floozutter.site>
2021-04-01 21:06:33 -0700

    hmm

--- java/TwoMeals/Hmm.java
+++ java/TwoMeals/Hmm.java
@@ -1,4 +1,12 @@
+// https://stackoverflow.com/a/19142321
+
 public class Hmm {
+    public static void main(String[] main) {
+        K a = (K)(new M());
+        a.p();
+        L b = (L)(new N());
+        b.q();
+    }
     private static interface K {
         public void p();
     }
@@ -6,4 +14,8 @@ public class Hmm {
         //public void p() { K o = new M(); }
         public void p() { K o = (K)(new M()); }
     }
+    private static interface L {
+        public void q();
+    }
+    private static class N {}
 }
