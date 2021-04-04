94703fec15a975d3013531bfab144764ddbaca7d
Floozutter <me@floozutter.site>
2021-04-04 03:06:17 -0700

    add bhufilltime.py

---
+++ python/bhufilltime.py
@@ -0,0 +1,9 @@
+"""
+calculate the time to fill an Industrial Foregoing Black Hole Unit!
+"""
+
+SLOTS = 2_147_483_647
+
+def seconds_from_slots_per_second(slots_per_second: float) -> float:
+    return SLOTS / slots_per_second
+
