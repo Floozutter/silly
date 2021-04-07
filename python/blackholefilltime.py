27fa9e32e55921fe11ab3e569c1b068e750d07f8
Floozutter <me@floozutter.site>
2021-04-07 02:54:18 -0700

    add fraction

--- python/blackholefilltime.py
+++ python/blackholefilltime.py
@@ -4,5 +4,8 @@ calculate the time to fill an Industrial Foregoing Black Hole Unit or Tank!
 
 SLOTS = 2_147_483_647
 
+def fraction(slots_filled: int) -> float:
+    return slots_filled / SLOTS
+
 def seconds(slots_per_second: float) -> float:
     return SLOTS / slots_per_second
