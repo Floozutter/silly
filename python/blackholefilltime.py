fd95d161eacbca9152b6b7133f88aca566c597ca
Floozutter <me@floozutter.site>
2021-04-07 02:53:11 -0700

    rename seconds_from... to seconds

--- python/blackholefilltime.py
+++ python/blackholefilltime.py
@@ -4,5 +4,5 @@ calculate the time to fill an Industrial Foregoing Black Hole Unit or Tank!
 
 SLOTS = 2_147_483_647
 
-def seconds_from_slots_per_second(slots_per_second: float) -> float:
+def seconds(slots_per_second: float) -> float:
     return SLOTS / slots_per_second
